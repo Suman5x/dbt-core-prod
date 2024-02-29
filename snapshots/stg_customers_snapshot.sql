45eaf4c6217049263844222f80be36b888209349
root <root@ip-10-0-1-154.ec2.internal>
2024-02-29 05:34:00 +0000

    commit

--- snapshots/stg_customers_snapshot.sql
+++ snapshots/stg_customers_snapshot.sql
@@ -1,16 +1,6 @@
 
 {% snapshot stg_orders_snapshot %}
 
-{{
-    config(
-      target_database='rawdata_db',  -- Change this to your actual target database
-      target_schema='snapshot',
-       -- Assuming 'o_orderkey' is the primary key in your orders model
-
-      strategy='timestamp'
-       -- Replace 'o_orderdate' with the actual timestamp column if different
-    )
-}}
 
 SELECT * FROM {{ ref('stg_customer') }}
 
--- snapshots/stg_supplier_snapshot.sql
+++ snapshots/stg_supplier_snapshot.sql
@@ -1,15 +1,5 @@
 {% snapshot stg_orders_snapshot %}
 
-{{
-    config(
-      target_database='rawdata_db',  -- Change this to your actual target database
-      target_schema='snapshot',
-       -- Assuming 'o_orderkey' is the primary key in your orders model
-
-      strategy='timestamp'
-       -- Replace 'o_orderdate' with the actual timestamp column if different
-    )
-}}
 
 SELECT * FROM {{ ref('stg_supplier') }}
 
